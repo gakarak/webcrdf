af99edbb16c558765dcdcc40a934c51082432746
Alexander Kalinovsky (UIIP, arpc) <gakarak@gmail.com>
2015-12-27 23:21:27 +0300

    fix run script

--- run_server_webcrdf.sh
+++ run_server_webcrdf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/.profile
+
 ##source ~/dev/bin/set-ocv-cuda-jdk-qt4.sh
 source ~/dev/bin/set-ocv-qt4.sh
 
@@ -8,4 +10,7 @@ source $HOME/venv/venv-pycharm/bin/activate
 cd webcrdf
 
 ##python manage.py runserver 192.168.0.10:8001
-python manage.py runserver 192.168.219.12:8001
+##python manage.py runserver 192.168.219.12:8001
+##python manage.py runserver 80.94.162.115:8080
+python manage.py runserver 127.0.0.1:8080 --insecure
+
